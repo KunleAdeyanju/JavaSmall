2a9a9fd7c3f995284711ad3dd485001a31ada125
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-02-27 19:09:24 -0500

    Section 0 and 1 comeplet of JavaSmall.md

---
+++ twoParameter.java
@@ -0,0 +1,25 @@
+int i = 1;
+ while(i <=10){System.out.println(i); i++;}
+void printNumbers(){ int i = 1; while (i<=10){ System.out.println(i); i++;}}
+printNumbers()
+void printNumbers(int n){ int i = 1; while (i<=n){ System.out.println(i); i++;}}
+printNumbers(20)
+printNumbers(5)
+int sumNumbers(int n){ int sum = 0; for(int i = 1; i <= n; i++){ sum = sum +i;} return sum;}
+ int sumNumbersTo10(){return sumNumbers(10);}
+sumNumbersTo10()
+sumNumbersTo10()
+int sumNumbersTo13(){return sumNumbers(13);}
+sumNumbersTo13()
+int addTwoNumber(int x, int y){ return x+y;}
+addTwoNumber(3,5)
+addTwoNumber(4,5)
+int divideTwoNumbers(int x, int y){return x/y;}
+int multiplyTwoNumbers(int x, int y){return x*y;}
+int subtractTwoNumbers( int x, int y){return x - y;}
+addTwoNumber(5,10)
+addTwoNumber(5,10);
+divideTwoNumbers(10,5)
+divideTwoNumbers(10,5);
+multiplyTwoNumbers(5,10);
+subtractTwoNumbers(10,5);
